f2fe6228e1b1da1cb36b44d498a2154bb8b4d918
wootaiklee <fined000@naver.com>
2024-02-19 22:54:15 +0900

    shared components2 done

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import './App.css'
 import styled from '@emotion/styled'
 import Text from './components/shared/Text'
 import Button from './components/shared/Button'
+import Input from './components/shared/Input'
+import TextField from './components/shared/TextField'
+import Alert from './components/shared/Alert'
+import { useAlertContext } from './contexts/AlertContext'
 
 function App() {
+  const { open }: any = useAlertContext()
   return (
     <div>
       <Text typography="t1" display="block" color="red">
@@ -14,6 +19,8 @@ function App() {
       <Text typography="t3">t3</Text>
       <Text typography="t4">t4</Text>
       <Text typography="t5">t5</Text>
+      <Button color="success">Primary</Button>
+      <Button weak={true}>Success</Button>
 
       <div
         style={{
@@ -22,9 +29,28 @@ function App() {
           background: '#efefef',
         }}
       >
-        <Button color="success">Primary</Button>
-        <Button weak={true}>Success</Button>
+        <Input placeholder="로그인" aria-invalid={false} />
+        <Input aria-invalid={true} />
+        <TextField label="이메일" />
+        <TextField label="패스워드" hasError={true} />
       </div>
+      {/* <Alert
+        title="알림"
+        description="테스트용"
+        onButtonClick={() => {}}
+        open={true}
+      /> */}
+      <Button
+        onClick={() => {
+          open({
+            title: '알림',
+            description: '테스트용',
+            onButtonClick: () => {},
+          })
+        }}
+      >
+        Alert 오픈
+      </Button>
     </div>
   )
 }
