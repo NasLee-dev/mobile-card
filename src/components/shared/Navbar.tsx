1afb6d12b9aaac1fbe84c22d0c2ba19f0d5b882e
wootaiklee <fined000@naver.com>
2024-03-03 17:39:02 +0900

    프로필 사진 업데이트 로직 완료

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -7,6 +7,7 @@ import useUser from '@/hooks/auth/useUser'
 import { useCallback } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '@/remote/firebase'
+import MyImage from '../my/MyImage'
 
 export default function Navbar() {
   const location = useLocation()
@@ -19,13 +20,9 @@ export default function Navbar() {
   const renderButton = useCallback(() => {
     if (user != null) {
       return (
-        <Button
-          onClick={() => {
-            handleLogout()
-          }}
-        >
-          로그아웃
-        </Button>
+        <Link to="/my">
+          <MyImage size={40} />
+        </Link>
       )
     }
     if (showSignButton) {
